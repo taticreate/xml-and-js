802f0615a282f295e5ffdfef26e0d91f2b6b947b
taticreate <42063764+taticreate@users.noreply.github.com>
2022-01-11 16:07:36 -0500

    Update evaluation-2-tati.js

--- week-1/assignments/evaluation-2-tati.js
+++ week-1/assignments/evaluation-2-tati.js
@@ -1,1 +1,5 @@
-
+function buttonFunction() {
+    document.getElementById('email').innerHTML = document.myform.email.value;
+    document.getElementById('date').innerHTML = document.myform.date.value;
+    document.getElementById('comment').innerHTML = document.myform.comment.value;
+}
